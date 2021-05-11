1d1c8e1f1cd51ea2593d76599b4e5d10e32a2fd1
satish-rajnale <satishrajnale98@gmail.com>
2021-05-11 09:44:47 +0530

    generic classes

--- database.ts
+++ database.ts
@@ -2,31 +2,89 @@
 
 // alot of time people are asked to implement a in memory nosqldb/  databse structure
 
-interface Database{
-    get(id:string): string;
-    set(id:string, value:string): void;
-}
+// interface Database {
+//   get(id: string): string;
+//   set(id: string, value: string): void;
+// }
 
-// persistable means you can read & write to your state through a string
+// // persistable means you can read & write to your state through a string
+// interface Persistable {
+//   saveToString(): string;
+//   restoreFromString(stroredState: string): void;
+// }
+
+// class InMemoryDatabase implements Database {
+//   //   private
+//   protected db: Record<string, string> = {};
+
+//   get(id: string): string {
+//     return this.db[id];
+//   }
+//   set(id: string, value: string): void {
+//     this.db[id] = value;
+//   }
+// }
+
+// const myDB = new InMemoryDatabase();
+
+// myDB.set("name", "Allen");
+// //myDB.db["gk"] = "Gomo" // db is not private this statement will execute
+// //console.log(myDB.get("gk"));
+
+// class PersitableDatabase extends InMemoryDatabase implements Persistable {
+//   saveToString(): string {
+//     return JSON.stringify(this.db); // if db is private wont be accessible here even if class extends
+//   }
+//   restoreFromString(stroredState: string): void {
+//     this.db = JSON.parse(stroredState);
+//   }
+// }
+
+// const myPersistantDB = new PersitableDatabase();
+
+// myPersistantDB.set("name", "Allen");
+// console.log(myPersistantDB.saveToString());
+
+// const savedState = myPersistantDB.saveToString();
+
+// const myDB2 = new PersitableDatabase();
+// myDB2.restoreFromString(savedState);
+
+// console.log(myDB2.get("name"));
+
+
+
+
+
+// same as above but generic version
+interface Database<K, T> {
+  get(id: K): T;
+  set(id: K, value: T): void;
+}
+// as just setting the id to K it doesnt knows what type to give it can accept either string
+// , number or sybmol but as we havent yet defined it it can take { } as a type also which is
+// not what we wanted
+// so use DBKeyType snd assign it to K when using as extended
+
+
+
 interface Persistable {
- saveToString(): string;
- restoreFromString(stroredState: string):void
+  saveToString(): string;
+  restoreFromString(stroredState: string): void;
 }
 
+type DBKeytype = string | number | symbol;
 
+class InMemoryDatabase<K extends DBKeytype, T> implements Database<K,T> {
+  //   private
+  protected db: Record<K, T> = {} as Record<K,T>
 
-
-class InMemoryDatabase implements Database {
-//   private  
-protected db: Record<string, string> = {};
-    
-    get(id: string): string {
-        return this.db[id]
-    };
-    set(id:string, value:string): void{
-        this.db[id] = value
-    }
-
+  get(id: K): T {
+    return this.db[id];
+  }
+  set(id: K, value: T): void {
+    this.db[id] = value;
+  }
 }
 
 const myDB = new InMemoryDatabase();
@@ -35,20 +93,18 @@ myDB.set("name", "Allen");
 //myDB.db["gk"] = "Gomo" // db is not private this statement will execute
 //console.log(myDB.get("gk"));
 
-
-class PersitableDatabase extends InMemoryDatabase implements Persistable{
-    saveToString(): string{
-        return JSON.stringify(this.db); // if db is private wont be accessible here even if class extends
-    }
-    restoreFromString(stroredState: string):void {
-        this.db = JSON.parse(stroredState)
-    }
+class PersitableDatabase<K extends DBKeytype, T> extends InMemoryDatabase<K,T> implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db); // if db is private wont be accessible here even if class extends
+  }
+  restoreFromString(stroredState: string): void {
+    this.db = JSON.parse(stroredState);
+  }
 }
 
+const myPersistantDB = new PersitableDatabase<string,number>();
 
-const myPersistantDB = new PersitableDatabase();
-
-myPersistantDB.set("name", "Allen");
+myPersistantDB.set("score",3456);
 console.log(myPersistantDB.saveToString());
 
 const savedState = myPersistantDB.saveToString();
@@ -56,6 +112,4 @@ const savedState = myPersistantDB.saveToString();
 const myDB2 = new PersitableDatabase();
 myDB2.restoreFromString(savedState);
 
-console.log(myDB2.get("name"))
-
-
+console.log(myDB2.get("score"));
