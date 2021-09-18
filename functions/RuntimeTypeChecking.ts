18b2a69b50114340e1c9cd88fac8646fbe90a7cb
satish-rajnale <satishrajnale98@gmail.com>
2021-09-18 12:20:01 +0530

    typecheck with zod

--- functions/RuntimeTypeChecking.ts
+++ functions/RuntimeTypeChecking.ts
@@ -1,19 +1,36 @@
 import fs from "fs";
+import { z } from "zod";// can use joi or yup also
 
-interface Result {
-    results: {
-        id: number,
-        name:string,
-        job:string
-    }[];
-}
+const ResultSchema = z.object({
+    results : z.array(
+        z.object({
+            id: z.number().min(100), // meaning must be 100 or more
+            name: z.string(),
+            job: z.string()
+        })
+    )
+});
+
+
+//you want to have a runtime check with resultschema but also want a compile time check with interface result so use z.infer
+type Result = z.infer<typeof ResultSchema>;
+
+
+// interface Result {
+//     results: {
+//         id: number,
+//         name:string,
+//         job:string
+//     }[];
+// }
 
 const printJobs = (results: Result) => {
-    results.results.forEach(({job}) => {
+    
+    results?.results?.forEach(({job}) => { // doing ? will not print anything as it has no else check on it 
         console.log(job);
     })
 };
-// this wont throw an err
+// this wont throw an err at runtime without ?
 // printJobs({
 //     results:[{
 //         id:1,
@@ -25,3 +42,15 @@ const printJobs = (results: Result) => {
 const data: Result = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 printJobs(data);// this here shows no error even if data.json has no balue resembling Result
 //will throw runtime error
+
+
+const printJobswithZod = (results: Result) => {
+    if(ResultSchema.safeParse(results).success){
+    results?.results?.forEach(({job}) => {
+        console.log(job);
+    })
+}else{
+    console.log("Bad data")
+}
+};
+printJobswithZod(data);
