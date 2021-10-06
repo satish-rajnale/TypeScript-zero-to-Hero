f71b26968e0178441937e07f258260157c860c4a
satish-rajnale <satishrajnale98@gmail.com>
2021-10-07 00:16:52 +0530

    factory pattern

--- Im-memory_Database/index.ts
+++ Im-memory_Database/index.ts
@@ -34,3 +34,31 @@ pokemonDB.set({
 });
 
 console.log(pokemonDB.get("pikachu"));
+
+
+//factory pattern
+function createDatabase<T extends Pokemon>() { // create InMemoryDatabse class at runtime
+    //@ts-ignore
+    class InMemoryDatabse implements Database<T> {
+      private db: Record<string, T> = {};
+  
+      public set(newVal: T): void {
+        this.db[newVal.id] = newVal;
+      }
+      public get(id: string): T {
+        return this.db[id];
+      }
+    };
+  
+    return InMemoryDatabse;
+  }
+  
+  const PokemonDB = createDatabase();
+  const pokeDBObj = new PokemonDB();
+  pokeDBObj.set({
+    id: "raichu",
+    attack: 54,
+    defense: 20,
+  });
+  
+  console.log(pokeDBObj.get("raichu"));
