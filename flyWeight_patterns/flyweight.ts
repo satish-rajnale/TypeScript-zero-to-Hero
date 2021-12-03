157295a8bd8af223f53af3e1763792c7f4593314
satish-rajnale <satishrajnale98@gmail.com>
2021-12-03 21:14:43 +0530

    Update flyweight.ts

--- flyWeight_patterns/flyweight.ts
+++ flyWeight_patterns/flyweight.ts
@@ -15,3 +15,39 @@ interface PokemonResults {
     url: string;
   }[];
 }
+
+function makeURLFlyweights<ReturnType>(urls: Record<string, string>) {
+  const myObject: Record<string, Promise<ReturnType>> = {};
+
+  return new Proxy(myObject, {
+    get: (target, name: string) => {
+      console.log(`Fetching ${name} from ${urls[name]}`);
+      if (!target[name]) {
+        target[name] = fetch(urls[name]).then((res) => res.json());
+      }
+      return target[name];
+    },
+  });
+}
+
+(async function () {
+  const data = (await (
+    await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+  ).json()) as PokemonResults;
+
+  //   console.log(data);
+
+  const urls = data.results.reduce(
+    (acc, { name, url }) => ({
+      ...acc,
+      [name]: url,
+    }),
+    {}
+  );
+
+  //   console.log(urls);
+
+  const lookup = makeURLFlyweights<Pokemon>(urls);
+  const mypokemon = await lookup.bulbasaur;
+  console.log(mypokemon);
+})();
