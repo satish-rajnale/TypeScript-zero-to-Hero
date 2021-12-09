1521d157b67bbda5dd43bf1ff765d51d0fb50c9b
satish-rajnale <satishrajnale98@gmail.com>
2021-12-09 22:07:46 +0530

    Create commands_with_classes.ts

---
+++ Commands_patterns/commands_with_classes.ts
@@ -0,0 +1,42 @@
+abstract class Command<State> {
+  abstract execute(state: State): State;
+  abstract undo(state: State): State;
+}
+
+class CommandStack<State> {
+  private stack: Command<State>[] = [];
+  constructor(private _state: State) {}
+
+  get state() {
+    return this._state;
+  }
+
+  execute(command: Command<State>) {
+    this._state = command.execute(this._state);
+    this.stack.push(command);
+  }
+
+  undo() {
+    const command = this.stack.pop();
+    if (command) {
+      this._state = command.undo(this._state);
+    }
+  }
+}
+
+class Addone extends Command<number> {
+  execute(state: number) {
+    return state + 1;
+  }
+
+  undo(state: number) {
+    return state - 1;
+  }
+}
+
+const cs = new CommandStack<number>(0);
+console.log(cs.state);
+cs.execute(new Addone());
+console.log(cs.state);
+cs.undo();
+console.log(cs.state);
